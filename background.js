6cf97d62284b7a115662049ed3507d6e59a06714
JonoKan2001 <74647687+JonoKan@users.noreply.github.com>
2022-03-01 21:35:20 +1300

    Initial commit

---
+++ background.js
@@ -0,0 +1,37 @@
+var imageWidth = 1;
+var previousImageWidth = 1;
+const websiteUrl = window.location.href;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    // Init tab: imageWidth = 1
+    // Other tab with different size: imageWidth > response
+    // Same tab
+
+    if(previousImageWidth < 0) {
+        // Change tab from facebook to other
+        previousImageWidth = imageWidth;
+        sendResponse(imageWidth);
+    } else {
+        previousImageWidth = request;
+        if(request < 0) {
+            imageWidth = Math.abs(request)/1.3;
+            console.log('Reduce size', imageWidth)
+            sendResponse(imageWidth);    
+        } else {
+            imageWidth = Math.max(Math.abs(request), imageWidth)
+            sendResponse(imageWidth);
+        }
+
+    }
+})
+
+//chrome.browserAction.onClicked
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // for the current tab, inject the "inject.js" file & execute it
+    chrome.tabs.executeScript(tab.ib,
+        {
+            file: 'inject.js'
+        }
+    );
+}
+);
+
---
+++ inject.js
@@ -0,0 +1,96 @@
+(function(){
+    var imageElement = document.createElement("img")
+    var currentWidth = 1;
+    imageElement.src = "https://media4.giphy.com/media/5be8JLZXiYQ4ubA90Q/giphy.gif?cid=790b7611e9610dc9fab969cea56687e9c045d92a5430e097&rid=giphy.gif&ct=s"
+    imageElement.id = "duckycursor"
+    imageElement.width = 25
+    imageElement.style.position = "fixed"
+    imageElement.style.pointerEvents = "none"
+    imageElement.style.zIndex = "9999"
+
+    document.body.append(imageElement)
+
+    document.body.style.cursor = "none"
+
+
+    var mouseX = (event) =>
+    {
+        return event.clientX
+    }
+    var mouseY = (event) =>
+    {
+        return event.clientY
+    }
+
+    var ducky = document.getElementById("duckycursor")
+
+    document.body.addEventListener('mousemove', e => {
+        ducky.style.top = mouseY(e) - imageElement.height/2 + "px"
+        ducky.style.left = mouseX(e) - imageElement.width/2 + "px"
+    })
+
+    //alert('You are about to be ducked!');
+    // Init size
+    chrome.runtime.sendMessage(currentWidth
+        , function (response) {
+    currentWidth = response;
+    imageElement.style.transform = "scale("+ currentWidth + ")"})
+
+    //animation for size increase
+
+    imageElement.style.transition =  "transform 500ms";
+    // For every click
+    document.body.addEventListener('click', e =>{
+        if (currentWidth < 70 && currentWidth >= 1) {
+
+            const websiteUrl = window.location.href;
+            console.log('Current website', websiteUrl)
+
+
+            if(websiteUrl.includes('facebook')) {
+                currentWidth = -currentWidth;
+            } else {
+                currentWidth *= 1.1;
+
+            }
+
+            chrome.runtime.sendMessage(currentWidth
+                , function (response) {
+            currentWidth = response;
+            imageElement.style.transform = "scale("+ currentWidth + ")"})
+        }
+    })
+
+    document.addEventListener("visibilitychange", function() {
+        if (document.hidden){
+            console.log("Browser tab is hidden")
+        } else {
+            console.log('Current width', currentWidth)
+                chrome.runtime.sendMessage(currentWidth
+                    , function (response) {
+                currentWidth = response;
+                imageElement.style.transform = "scale("+ currentWidth + ")"})
+        }
+    });
+
+    // grows instantly after one click on canvas 
+    if (window.location.href.indexOf("auckland.ac.nz") != -1) {
+        imageElement.src = "https://media1.giphy.com/media/HVL15iRon8yh17iwfV/giphy.gif?cid=790b761129f13c240b46038a13120cbe7cc067b51f2fecc8&rid=giphy.gif&ct=s"
+        imageElement.style.transition =  "transform 500ms"
+        currentWidth *= 20;
+        imageElement.style.transform = "scale("+ currentWidth + ")"
+    }
+
+    // play quack sound on click
+    var audio = new Audio("https://wavlist.com/wav/duck-quack3.wav");
+    document.onclick = function() {
+        audio.play();
+    }
+
+})();
+
+// to do
+/*find out how to get it running without having to open the extension pop up
+
+add condition for running it differently depending on the website */
+
